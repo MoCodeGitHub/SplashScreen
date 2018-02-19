25909bf21ae9171a5a6f9798fac1c5ee924689a3
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2018-02-19 01:12:00 +0100

    Created App background

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Image source={require('./img/bg.gif')} style={styles.container}>
+
+      </Image>
     );
   }
 }
@@ -16,8 +14,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
+    alignSelf: 'stretch'
+    },
 });
